be2cb0aadc3b3883da8507666beaa4f4442427a7
Alex Wilson <acwilson96@protonmail.com>
2017-10-02 14:16:39 +0100

    Base App Component that routes between Loading, Login and Main App page.

---
+++ src/views/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Spinner from 'react-spinkit';
+import Login from './Login';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      authToken: null
+    }
+  }
+
+  componentDidMount = () => {
+    // Try get the authToken if it exists.
+    var authToken = localStorage.getItem('authToken');
+    console.log(authToken);
+    // Mock API.
+    setTimeout(() => { this.setState({loading: false}); }, 5000);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className="wrapper" align="center">
+          <div className="middle">
+            <Spinner name="cube-grid" />        
+          </div>
+        </div>
+      );
+    }
+    else {
+      if (this.state.authToken != null) {
+        return (
+          <div>
+            This component will host the logic to determine which view to show.
+          </div>
+        );
+      }
+      else {
+        return (
+          <Login />          
+        );
+      }
+    }
+  }
+}
+
+export default App;
