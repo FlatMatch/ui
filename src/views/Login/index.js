39458f7b172952f7c9dd3b5c20edd69771ec46e8
Alex Wilson <acwilson96@protonmail.com>
2017-10-02 22:46:10 +0100

    Login page structure.

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -5,7 +5,23 @@ export default class Login extends Component {
   render() {
     return (
       <div className="login-container">
-        Login Page
+        <div className="login-header" align="center">
+          Login
+        </div>
+        <div className="login-body">
+          <div align="center">
+            <input id="login_email" type="text" placeholder="email@sms.ed.ac.uk" size="20"></input>
+          </div>
+          <br/>
+          <div align="center">
+            <input id="login_pword" type="password" placeholder="password"></input>
+          </div>
+          <br/>
+          <div align="center">
+            <div className="login-button">login</div>
+          </div>
+        </div>
+        <a className="register-link">register</a>
       </div>
     );
   }
