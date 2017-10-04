fab2705a1359bfb4d6c85f0e971fc6ca1572996d
Alex Wilson <acwilson96@protonmail.com>
2017-10-05 00:32:56 +0100

    Navbar Component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../styles/Navbar.css';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <div className="navbar-container">
+        Flat Match
+      </div>
+    );
+  }
+}
