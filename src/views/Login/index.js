b0aa6117347af3eee642ea9943caf43df5fe55cf
Alex Wilson <acwilson96@protonmail.com>
2017-10-07 14:24:31 +0100

    Login View/Component now passes username/password up to App Component.

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -3,12 +3,28 @@ import '../../styles/Login.css';
 
 export default class Login extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
   login = () => {
-    alert("@TODO Logging In");
+    this.props.login(this.state.username, this.state.password);
   }
 
   toggleRegister = () => {
     this.props.toggleRegister();
+  }
+
+  updateUsername = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  updatePassword = (event) => {
+    this.setState({password: event.target.value});
   }
 
   render() {
@@ -19,11 +35,11 @@ export default class Login extends Component {
         </div>
         <div className="login-body">
           <div align="center">
-            <input id="login_email" type="text" placeholder="email@sms.ed.ac.uk" size="20"></input>
+            <input id="login_email" onChange={this.updateUsername} type="text" placeholder="email@sms.ed.ac.uk" size="20"></input>
           </div>
           <br/>
           <div align="center">
-            <input id="login_pword" type="password" placeholder="password"></input>
+            <input id="login_pword" onChange={this.updatePassword} type="password" placeholder="password"></input>
           </div>
           <br/>
           <div align="center">
