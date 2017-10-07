584f7df8609ab1b7c695252874da93e3f45a9f85
Alex Wilson <acwilson96@protonmail.com>
2017-10-07 14:24:55 +0100

    Added networking helper for posting to API. (Dummy for now).

--- src/helpers/networking.js
+++ src/helpers/networking.js
@@ -1,9 +1,18 @@
 module.exports = {
 
     /* Checks if an authToken is still valid. */
+    /* Callback with validity: [ true, false ] */
     verifyAuthToken: (authToken, cb) => {
         // @TODO - Mock API returns that token is always valid.
         setTimeout(() => cb(true), 2000);
+    },
+
+    /* Submits username and password to backend. */
+    /* Callback with authToken: [ null, authToken ] */
+    postLogin: (username, password, cb) => {
+        setTimeout(() => {
+            cb(null, 'dummyAuthToken');
+        }, 3000);
     }
 
 }
