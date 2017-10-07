c6151eb16aa9afcbf9fc618adeac5c1005bedcce
Alex Wilson <acwilson96@protonmail.com>
2017-10-07 15:07:45 +0100

    Register Component supports registering via App.

--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -3,12 +3,38 @@ import '../../styles/Register.css';
 
 export default class Register extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: ''
+    }
+  }
+
   register = () => {
-    alert("@TODO Registering");
+    this.props.register(this.state.first_name, this.state.last_name, this.state.email, this.state.password);
   }
 
   toggleRegister = () => {
     this.props.toggleRegister();
+  }
+
+  updateFirstname = (event) => {
+    this.setState({first_name: event.target.value});
+  }
+
+  updateLastname = (event) => {
+    this.setState({last_name: event.target.value});
+  }
+  
+  updateEmail = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  updatePassword = (event) => {
+    this.setState({password: event.target.value});
   }
 
   render() {
@@ -19,19 +45,19 @@ export default class Register extends Component {
         </div>
         <div className="register-body">
           <div align="center">
-            <input id="register_fname" type="text" placeholder="First Name"></input>
+            <input id="register_fname" onChange={this.updateFirstname} type="text" placeholder="First Name"></input>
           </div>
           <br/>
           <div align="center">
-            <input id="register_lname" type="text" placeholder="Last Name"></input>
+            <input id="register_lname" onChange={this.updateLastname} type="text" placeholder="Last Name"></input>
           </div>
           <br/>
           <div align="center">
-            <input id="register_email" type="text" placeholder="email@sms.ed.ac.uk" size="20"></input>
+            <input id="register_email" onChange={this.updateEmail} type="text" placeholder="email@sms.ed.ac.uk" size="20"></input>
           </div>
           <br/>
           <div align="center">
-            <input id="register_pword" type="password" placeholder="password"></input>
+            <input id="register_pword" onChange={this.updatePassword} type="password" placeholder="password"></input>
           </div>
           <br/>
           <div align="center">
